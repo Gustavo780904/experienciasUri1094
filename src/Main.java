9b6df621a85aeeabbf821c5bfac19cca0e0ff40c
Gustavo780904 <gustavodutrasantos@gmail.com>
2020-06-05 23:32:17 -0300

    Leitura dos dados

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int N, quantia, totalCobaias;
+		double percentualC, percentualR, percentualS;
+		
+		N = sc.nextInt();
+		
+		
+		
+		sc.close();
+
+	}
+
+}
